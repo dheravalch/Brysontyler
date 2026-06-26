00c1af94542a469e01385a39c8386402035beab1
perfectionchiz <perfectionchizuruoke@gmail.com>
2026-06-26 02:12:04 -0700

    updated

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,11 +3,24 @@ const config = {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
       tagCache: "dummy",
       queue: "dummy",
     },
   },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  edgeExternals: ["node:crypto"],
 };
 
 export default config;
